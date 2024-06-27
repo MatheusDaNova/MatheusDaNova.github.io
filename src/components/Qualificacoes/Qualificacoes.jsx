4c8bef0bacad804c28ce614adbd07538b388c1fe
FláviaEduarda <mpereiranova@gmail.com>
2024-06-26 22:38:06 -0300

    qualificacoes

--- src/components/Qualificacoes/Qualificacoes.jsx
+++ src/components/Qualificacoes/Qualificacoes.jsx
@@ -9,12 +9,6 @@ const Qualificacoes = () => {
 
             <div className="qualificacao__container container">
                 <div className="qualificacao__tabs">
-                    <div></div>
-
-                    <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                    </div>
 
                     <div className="qualificacao__button qualificacao__active button--flex">
                         <i className="uil uil-graduation-cap qualificacao__icon"></i>{""} Formação 
@@ -29,7 +23,7 @@ const Qualificacoes = () => {
                     <div className="qualification__content">
                         <div className="qualification__data">
                             <div>
-                                <h3 className="qualification__title"></h3>
+                                <h3 className="qualification__title">Engenharia Eletrônica</h3>
                                 <span className="qualification__subtitle">CEFET</span>
                                 <div className="qualification__calendar">
                                     <i className="uil uil-calendar-alt">2023 - Atualmente</i>
@@ -40,6 +34,25 @@ const Qualificacoes = () => {
                             <span className="qualification__rounder"></span>
                             <span className="qualification__line"></span>
                         </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Desenvolvimento de Sistemas</h3>
+                                <span className="qualification__subtitle">Colégio Pedro II</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">2019 - 2022</i>
+                                </div>
+                            </div>
+
 
                         </div>
                     </div>
